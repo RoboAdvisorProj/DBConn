f0eea6c8f9f48061c605fe7ee425829fee65f63a
sonhs7717 <34508836+sonhs7717@users.noreply.github.com>
2018-01-28 16:15:23 +0900

    commit

--- stockset_basic_fixed.sql
+++ stockset_basic_fixed.sql
@@ -28,9 +28,9 @@ CREATE TABLE `basic_fixed` (
   `NIY2` varchar(10) NOT NULL,
   `NIY1` varchar(10) NOT NULL,
   `NIY0` varchar(10) NOT NULL,
-  `SALEY2` varchar(10) NOT NULL,
-  `SALEY1` varchar(10) NOT NULL,
-  `SALEY0` varchar(10) NOT NULL,
+  `SALEY2` float NOT NULL,
+  `SALEY1` float NOT NULL,
+  `SALEY0` float NOT NULL,
   `AST` varchar(10) NOT NULL,
   `CAP` varchar(10) NOT NULL,
   `DPT` varchar(10) NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE `basic_fixed` (
 
 LOCK TABLES `basic_fixed` WRITE;
 /*!40000 ALTER TABLE `basic_fixed` DISABLE KEYS */;
-INSERT INTO `basic_fixed` VALUES ('123456','KELOGG','25','50','80','5','6','7','10','6','4','1'),('123476','PERLABYSS','23','40','66','50','60','70','100','60','40','2'),('123422','Hospok','31','30','29','60','66','60','80','20','60','3');
+INSERT INTO `basic_fixed` VALUES ('123456','HOPE','5','11','20',14,555,5552,'50','18','32','1'),('123457','GOPE','500','700','300',412,5251,2512,'40','35','5','2'),('123458','HYNWOO','120','100','80',555,112,5251,'20','15','5','3'),('123459','SULGI','90','800','670',2525,1255,2555,'80','48','32','4');
 /*!40000 ALTER TABLE `basic_fixed` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-14  7:31:30
+-- Dump completed on 2018-01-28 16:14:24

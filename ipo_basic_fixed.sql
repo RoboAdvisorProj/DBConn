578c12d6663bef125976760d71c8dbaa1eb7a768
sonhs7717 <34508836+sonhs7717@users.noreply.github.com>
2018-02-11 16:27:26 +0900

    DB_table.v1.0
    commit

---
+++ ipo_basic_fixed.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ipo
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `basic_fixed`
+--
+
+DROP TABLE IF EXISTS `basic_fixed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `basic_fixed` (
+  `CODE` char(6) NOT NULL,
+  `Name` char(32) NOT NULL,
+  `NIY2` varchar(10) NOT NULL,
+  `NIY1` varchar(10) NOT NULL,
+  `NIY0` varchar(10) NOT NULL,
+  `SALEY2` float NOT NULL,
+  `SALEY1` float NOT NULL,
+  `SALEY0` float NOT NULL,
+  `AST` varchar(10) NOT NULL,
+  `CAP` varchar(10) NOT NULL,
+  `DPT` varchar(10) NOT NULL,
+  `Numb` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Numb`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `basic_fixed`
+--
+
+LOCK TABLES `basic_fixed` WRITE;
+/*!40000 ALTER TABLE `basic_fixed` DISABLE KEYS */;
+INSERT INTO `basic_fixed` VALUES ('123456','HOPE','5','11','20',2124,1244,7743,'50','18','32',1),('123457','GOPE','500','700','300',244,123,433,'40','35','5',2),('123458','HYNWOO','120','100','80',737,223,553,'20','15','5',3),('123459','SULGI','90','800','670',2533,412,664,'80','48','32',4);
+/*!40000 ALTER TABLE `basic_fixed` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-10 16:23:00
